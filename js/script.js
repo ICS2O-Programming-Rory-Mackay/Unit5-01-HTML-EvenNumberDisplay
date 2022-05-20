7a88cb4a84e4e1740e28de19e8b5ea3059555419
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-05-20 14:20:17 +0000

    fixed the trash

--- js/script.js
+++ js/script.js
@@ -10,25 +10,25 @@
  * This function displays if the user correctly guesses a random number
  */
 function displayNumber () {
-  
-  // initialize counter
-	let counter = Min
+	
+	// get user input for min
+	let min = parseInt(document.getElementById('min').value)
+
+	// get user input for max
+	let max = parseInt(document.getElementById('max').value)
+
+ // initialize counter
+	let counter = min
 
   // initialize numbers as empty string
   let numbers = ""
-	
-	// get user input for min
-	let numMin = parseInt(document.getElementById('min').value)
-
-	// get user input for max
-	let numMax = parseInt(document.getElementById('max').value)
-
+  
 	// WHILE loop
-	while ((counter >= min) && (counter <= max)) {
+	while (counter <= max) {
     // make number string with comma's
     numbers = numbers + counter + ", "
     // increment the counter
-    counter = counter++
+    counter++
   }
   // display sequence back to user
 	document.getElementById("message").innerHTML = numbers
